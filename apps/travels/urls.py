bebee2a23a6133d46a7624aba1bd6dcd6bf7299d
Matt Pedersen <mattpp@Matts-MacBook-Air.local>
2017-01-31 18:35:50 -0800

    pretty much finish all html/css

--- apps/travels/urls.py
+++ apps/travels/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # url(r'^register$', views.register, name='register'),
-    # url(r'^add$', views.add_travel, name='add_travel'),
+    url(r'^destination/(?P<id>\d+)$', views.show_travel, name='show_travel'),
+    url(r'^add$', views.add_travel, name='add_travel'),
     url(r'^$', views.index, name='index'),
 ]
--- apps/travels/views.py
+++ apps/travels/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
-    return render(request, 'travels/index.html')
+    context = {
+        'home': True
+    }
+    return render(request, 'travels/index.html', context)
+
+def show_travel(request, id):
+    return render(request, 'travels/show_travel.html')
+
+def add_travel(request):
+    return render(request, 'travels/new_travel.html')
